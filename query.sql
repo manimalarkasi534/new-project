f3c53ed46bffaa4210a961917b219227e8fb41b3
manimalarkasi534 <manimalarkasi.com>
2025-07-21 19:23:39 +0530

    Initial commit

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- drop database login;
+create database login;
+-- use login;
+-- create table Login (studentID  varchar(10),DateOfBith date);
+-- ALTER TABLE login
+-- CHANGE DtaetOfBirth DateOfBirth DATE;
+-- ALTER TABLE login RENAME COLUMN DateOfBith TO DateOfBirth;
+-- INSERT INTO login (studentID, DateOfBirth) VALUES ('24PC253923', '2003-03-05'),('24PC253966', '2001-10-01');
